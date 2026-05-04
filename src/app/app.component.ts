6cd50441c44300e16b18cce431850b86c0fe6948
bakrma7moud777 <bakr.ma7moud.777@gmail.com>
2026-05-04 19:43:05 +0300

    clean project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponentComponent } from './home-component/home-component.component';
 
 @Component({
   selector: 'app-root',
